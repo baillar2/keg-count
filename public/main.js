d5a4f68c02c5e3d8dbc18b0b6cdc61532747f3cd
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-12-16 16:10:55 -0700

    account

--- public/main.js
+++ public/main.js
@@ -1,4 +1,8 @@
 angular.module('app')
 	.controller('KegControl', ['$scope', '$http', function($scope, $http){
 		var s = $scope
+		s.accounts = [{name.'Little Maching', 
+					   contact.'Sean Baillargeon', 
+					   number.'517-980-4218',
+					   address.'nowhere town'	}]
 	}])
