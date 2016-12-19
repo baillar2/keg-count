01731d832a74e0e6d78e215b2e64ee72ed8c04e4
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-12-19 15:41:59 -0700

    created more controllers

--- public/main.js
+++ public/main.js
@@ -5,23 +5,35 @@ angular.module('app',[])
 		s.accounts = [{name:'Little Machine', 
 					   contact:'Sean Baillargeon', 
 					   number:'517-980-4218',
-					   address:'nowhere town'},{name:'Little Maching', 
+					   address:'nowhere town'},
+					   {name:'Little Maching', 
 					   contact:'Sean Baillargeon', 
 					   number:'517-980-4218',
 					   address:'nowhere town'}]
 		
-		s.beers = [
-		{name:'RacerBack', 
-		style:'English Pale Ale', 
-		abv:'5.0',
-		availability:'310'}, 
-		{name:'Tractor Beam', 
-		style:'Oatmeal Stout',
-		abv:'6.6',
-		availability:'155'
-		}]
+		
 		console.log(s.accounts, 'array')
 		console.log(s.beers, 'beers')
 	}])
 
+.controller('accountControl', ['$scope', '$http', function($scope, $http){
+	var s = $scope
+}])
+.controller('beerControl', ['$scope', '$http', function($scope, $http){
+	var s = $scope
+	s.beers = [{name:'RacerBack', 
+				style:'English Pale Ale', 
+				abv:'5.0',
+				availability:'310'}, 
+				{name:'Tractor Beam', 
+				style:'Oatmeal Stout',
+				abv:'6.6',
+				availability:'155'}]
 
+}
+.controller('addAccControl' ['$scope', '$http', function($scope, $http){
+	var s = $scope
+}])
+.controller('addBeerControl', ['$scope', '$http', function($scope, $http){
+	var s = $scope
+}])
